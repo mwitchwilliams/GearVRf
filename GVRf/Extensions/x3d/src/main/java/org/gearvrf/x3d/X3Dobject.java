48b071780b9384731682e0d2a3b177b6a259dcdc
Mitch Williams <m1.williams@partner.samsung.com>
2018-02-21 13:07:47 -0800

    correction in the JavaScript import statement

--- GVRf/Extensions/x3d/src/main/java/org/gearvrf/x3d/X3Dobject.java
+++ GVRf/Extensions/x3d/src/main/java/org/gearvrf/x3d/X3Dobject.java
@@ -606,7 +606,7 @@ public class X3Dobject {
     // Default is true to use Universal lights shader.
     public final static boolean UNIVERSAL_LIGHTS = true;
 
-    private final static String JAVASCRIPT_IMPORT_PACKAGE = "importPackage(org.gearvrf.x3d.data_types)\nimportPackage(org.joml)\nimportPackage(org.gearvrf)\nnimportPackage(org.gearvrf.x3ddemo)";
+    private final static String JAVASCRIPT_IMPORT_PACKAGE = "importPackage(org.gearvrf.x3d.data_types)\nimportPackage(org.joml)\nimportPackage(org.gearvrf)";
 
     // Strings appended to GVRScene names when there are multiple
     // animations on the same <Transform> or GVRSceneObject
